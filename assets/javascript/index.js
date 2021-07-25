e1d46f6cb86f1212813a9dd41bc967ae66b79abc
sln_ivan <ivan.slnnk@gmail.com>
2021-07-25 11:55:36 +0300

    dropdown

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -6,4 +6,19 @@ document.querySelector('.burger').addEventListener('click', function(){
 })
 document.querySelector('.burger').addEventListener('click', function () {
     document.querySelector('.menu').classList.toggle('active');
-  })
+})
+
+
+document.querySelector('.li').addEventListener('mouseover', function () {
+    document.querySelector('.dropdown-content').classList.add('show')
+})
+
+document.querySelector('.li').addEventListener('mouseout', function () {
+    document.querySelector('.dropdown-content').classList.remove('show')
+})
+// document.querySelector('.dropdown-content').addEventListener('mouseover', function () {
+//     this.classList.add('show')
+// })
+// document.querySelector('.dropdown-content').addEventListener('mouseout', function () {
+//     this.classList.remove('show')
+// })
